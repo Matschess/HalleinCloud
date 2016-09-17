4d61e923a3e3a2a7792986e767029cb8a4b251da
Matthias Lang <langmattis@gmail.com>
2016-09-17 04:06:42 +0200

    Design of index

--- index.php
+++ index.php
@@ -5,12 +5,37 @@
     <title></title>
     <link rel="stylesheet" href="assets/css/style.css" media="screen">
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+    <!-- Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   </head>
   <body>
-  <div class="navbar">
+  <div class="topbar">
     <h1>Verwaltungsdashboard</h1>
-
+    <div class="user">
+      Benutzername
+      <img src="assets/img/user.jpg"/>
+      <i class="material-icons">keyboard_arrow_down</i>
+    </div>
   </div>
+  <div class="navbar">
+    <ul>
+      <li>Link1</li>
+      <li>Link2</li>
+      <li>Link3</li>
+    </ul>
+  </div>
+  <div class="container">
+  <div class="dashboardBox">
+    <div class="notification alert">3</div>
+    <div class="title">Titel</div>
+    <div class="text">Text</div>
+  </div>
+  <div class="dashboardBox">
+    <div class="notification success"><i class="material-icons">done</i></div>
+    <div class="title">Titel</div>
+    <div class="text">Text</div>
+  </div>
+</div>
   </body>
 </html>
