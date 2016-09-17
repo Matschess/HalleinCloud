f7f747acaa29eef77cfc28d6abc1321f564d8910
Matthias Lang <langmattis@gmail.com>
2016-09-17 02:53:06 +0200

    Add index.php and style.css

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="assets/css/style.css" media="screen">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  </head>
+  <body>
+  <div class="navbar">
+    <h1>Verwaltungsdashboard</h1>
+
+  </div>
+  </body>
+</html>
