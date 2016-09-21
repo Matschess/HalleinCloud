6d6b4d458e2dae4b11f374c9a6fecba1e14b0230
Matthias Lang <Matthias Lang>
2016-09-22 00:05:44 +0200

    Improve design on dashboard - add nice animations to dashboard-boxes, actionbar and popup - improve integration of AngularJS

--- controllers/navigationController.js
+++ controllers/navigationController.js
@@ -1,13 +1,38 @@
-var myApp = angular.module('dashboard',[]);
+var myApp = angular.module('dashboard', ['ngSanitize', 'ngCookies']);
 
-myApp.controller('NavigationController', ['$scope', function($scope) {
-    $scope.links = ['Dashboard', 'Mahlzeiten', 'Feedback', 'Restaurantseite', 'Hilfe'];
-    $scope.openSettings = function() {
-        $('.overlay').show();
-        $('.popup').show();
+myApp.controller('NavigationController', function ($scope, $http, $cookies) {
+    $scope.links = [
+        {name: 'Dashboard', url: 'dashboard.html'},
+        {name: 'Mahlzeiten', url: 'food.html', action: 'save'},
+        {name: 'Feedback', url: 'feedback.html', action: 'save'},
+        {name: 'Restaurantseite', url: 'page.html', action: 'save'},
+        {name: 'Hilfe', url: 'help.html'}
+    ];
+    var url = $cookies.get('view');
+    $http.get('content/' + url)
+        .then(function (response) {
+            $scope.content = response.data;
+        });
+    $scope.action;
+
+
+    $scope.openSettings = function () {
+        $scope.popup = true;
     }
-    $scope.popupClose = function() {
-        $('.overlay').hide();
-        $('.popup').hide();
+    $scope.popupClose = function () {
+        $scope.popup = false;
     }
-}]);
+    $scope.load = function (url, action) {
+        if(action) {
+            $scope.action = true;
+        }
+        else {
+            $scope.action = false;
+        }
+        $http.get('content/' + url)
+            .then(function (response) {
+                $scope.content = response.data;
+                $cookies.put('view', url);
+            });
+    }
+});
