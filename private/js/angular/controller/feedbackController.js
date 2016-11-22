844e17718fe2e1dd15f74cf5b0884d16c2e7c7da
Matthias Lang <Matthias Lang>
2016-11-22 16:59:11 +0100

    Feedback is now updated in the DB on accept/decline

--- private/js/angular/controller/feedbackController.js
+++ private/js/angular/controller/feedbackController.js
@@ -5,35 +5,47 @@ myApp.controller('feedbackController', function ($scope, $http) {
         content: 'content/feedback.html'
     }
 
-    $http.get(URL + '/feedback?status=1')
+    $http.get(URL + '/feedback?get=id,rating,subject,text&status=1')
         .then(function (response) {
             $scope.newFeedbacks = response.data;
         })
 
-    $http.get(URL + '/feedback?status=2')
+    $http.get(URL + '/feedback?get=id,rating,subject,text&status=2')
         .then(function (response) {
             $scope.acceptedFeedbacks = response.data;
         })
 
-    $http.get(URL + '/feedback?status=3')
+    $http.get(URL + '/feedback?get=id,rating,subject,text&status=3')
         .then(function (response) {
             $scope.declinedFeedbacks = response.data;
         })
 
-    $scope.accept = function (index) {
-        $scope.acceptedFeedbacks.push($scope.newFeedbacks[index]);
-        $scope.newFeedbacks.splice(index, 1);
+    $scope.accept = function (id, index) {
+        $http.put(URL + '/feedback?status=2&id=' + id)
+            .then(function () {
+                $scope.acceptedFeedbacks.push($scope.newFeedbacks[index]);
+                $scope.newFeedbacks.splice(index, 1);
+            });
     }
-    $scope.decline = function (index) {
-        $scope.declinedFeedbacks.push($scope.newFeedbacks[index]);
-        $scope.newFeedbacks.splice(index, 1);
+    $scope.decline = function (id, index) {
+        $http.put(URL + '/feedback?status=3&id=' + id)
+            .then(function () {
+                $scope.declinedFeedbacks.push($scope.newFeedbacks[index]);
+                $scope.newFeedbacks.splice(index, 1);
+            });
     }
-    $scope.reDecline = function (index) {
-        $scope.declinedFeedbacks.push($scope.acceptedFeedbacks[index]);
-        $scope.acceptedFeedbacks.splice(index, 1);
+    $scope.reDecline = function (id, index) {
+        $http.put(URL + '/feedback?status=3&id=' + id)
+            .then(function () {
+                $scope.declinedFeedbacks.push($scope.acceptedFeedbacks[index]);
+                $scope.acceptedFeedbacks.splice(index, 1);
+            });
     }
-    $scope.reAccept = function (index) {
-        $scope.acceptedFeedbacks.push($scope.declinedFeedbacks[index]);
-        $scope.declinedFeedbacks.splice(index, 1);
+    $scope.reAccept = function (id, index) {
+        $http.put(URL + '/feedback?status=2&id=' + id)
+            .then(function () {
+                $scope.acceptedFeedbacks.push($scope.declinedFeedbacks[index]);
+                $scope.declinedFeedbacks.splice(index, 1);
+            });
     }
 });
